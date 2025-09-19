5c1133721156c43541a78512044acf66f51247c9
LTY <345014522@qq.com>
2025-09-20 01:39:12 +0800

    课程进度：技能系统(1)的代码

---
+++ Unity/Assets/Scripts/Codes/Model/Server/MMO/Cast.cs
@@ -0,0 +1,44 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ET.Server
+{
+    /// <summary>
+    /// 投射类技能
+    /// </summary>
+    [ChildOf(typeof(CastComponent))]
+    public class Cast: Entity,IAwake<int>,IDestroy
+    {
+        public int ConfigId;
+
+        [BsonIgnore]
+        public CastConfig Config
+        {
+            get
+            {
+                return CastConfigCategory.Instance.Get(this.ConfigId);
+            }
+        }
+
+        /// <summary>
+        /// 技能释放者
+        /// </summary>
+        [BsonIgnore]
+        public Unit Caster;
+
+        /// <summary>
+        /// 技能受击对象
+        /// </summary>
+        [BsonIgnore]
+        public List<long> Targets = new List<long>();
+
+        /// <summary>
+        /// 技能开始时间
+        /// </summary>
+        public long StartTime;
+    }
+}
---
+++ Unity/Assets/Scripts/Codes/Model/Server/MMO/CastComponent.cs
@@ -0,0 +1,36 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ET.Server
+{
+    /// <summary>
+    /// 释放技能的组件，角色调用此组件释放技能
+    /// </summary>
+    [ComponentOf(typeof(Unit))]
+    public class CastComponent : Entity, IAwake, IDestroy
+    {
+        //public int ConfigId;
+
+        //[BsonIgnore]
+        //public CastConfig Config
+        //{
+        //    get
+        //    {
+        //        return CastConfigCategory.Instance.Get(this.ConfigId);
+        //    }
+        //}
+
+
+        //[BsonIgnore]
+        //public Unit Caster;//技能释放者
+
+        //[BsonIgnore]
+        //public List<long> Targets = new List<long>();//技能受击对象
+
+        //public long StartTime;//技能开始时间
+    }
+}
