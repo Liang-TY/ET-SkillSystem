cf568b0f5cde945b9a59349a734401621e2edbb3
LTY <345014522@qq.com>
2026-08-25 14:29:40 +0800

    填4面板拖动stub实现（两阶段第二阶段，格式修正）

--- Packages/cn.etetet.lockstep/Scripts/HotfixView/Client/YIUISystem/Lobby/RoleInfoPanelComponentSystem.cs
+++ Packages/cn.etetet.lockstep/Scripts/HotfixView/Client/YIUISystem/Lobby/RoleInfoPanelComponentSystem.cs
@@ -48,7 +48,8 @@ namespace ET.Client
         [YIUIInvoke(RoleInfoPanelComponent.OnEventWindowDragInvoke)]
         private static async ETTask OnEventWindowDragInvoke(this RoleInfoPanelComponent self)
         {
-            
+            // 拖动窗口（按下标题区）：WindowDragComponent 逐帧移动，松手结束
+            WindowDragHelper.Begin(self.Root(), self.u_ComWindow);
             await ETTask.CompletedTask;
         }
         #endregion YIUIEvent结束
