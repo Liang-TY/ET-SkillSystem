984c13ceddd5da34cd1cb78543ef30800694d4e7
LTY <345014522@qq.com>
2026-08-25 17:34:11 +0800

    血条代码改锚点拉伸法，面板引用容器u_ComMonsterHpBar

--- Packages/cn.etetet.lockstep/Scripts/HotfixView/Client/YIUISystem/Battle/BattleInfoPanelComponentSystem.cs
+++ Packages/cn.etetet.lockstep/Scripts/HotfixView/Client/YIUISystem/Battle/BattleInfoPanelComponentSystem.cs
@@ -1,7 +1,5 @@
 using System;
-using TrueSync;
 using UnityEngine;
-using UnityEngine.UI;
 using YIUIFramework;
 using System.Collections.Generic;
 
@@ -19,12 +17,6 @@ namespace ET.Client
         private static void YIUIInitialize(this BattleInfoPanelComponent self)
         {
             self.HideMonster();
-
-            // 保险：确保两条血条 Image 为 Filled 类型（fillAmount 只在 Filled 下有视觉效果）
-            self.u_ComImgPlayerHp.type = Image.Type.Filled;
-            self.u_ComImgPlayerHp.fillMethod = Image.FillMethod.Horizontal;
-            self.u_ComImgMonsterHp.type = Image.Type.Filled;
-            self.u_ComImgMonsterHp.fillMethod = Image.FillMethod.Horizontal;
         }
 
         [EntitySystem]
@@ -42,31 +34,39 @@ namespace ET.Client
         #region YIUIEvent开始
         #endregion YIUIEvent结束
 
-        /// <summary>显示怪物信息（名字+血条框架）</summary>
+        /// <summary>显示怪物信息（名字+血条容器）</summary>
         public static void ShowMonster(this BattleInfoPanelComponent self, string name)
         {
             self.u_ComTextMonsterName.text = name;
             self.u_ComTextMonsterName.gameObject.SetActive(true);
-            self.u_ComImgMonsterHp.gameObject.SetActive(true);
+            self.u_ComMonsterHpBar.gameObject.SetActive(true);
         }
 
-        /// <summary>设置玩家血条比例（0~1）</summary>
+        /// <summary>设置玩家血条比例（0~1，锚点拉伸法：anchorMax.x 控制填充宽度）</summary>
         public static void SetPlayerHpRatio(this BattleInfoPanelComponent self, float ratio)
         {
-            self.u_ComImgPlayerHp.fillAmount = Mathf.Clamp01(ratio);
+            RectTransform fill = self.u_ComImgPlayerHp.transform as RectTransform;
+            if (fill != null)
+            {
+                fill.anchorMax = new Vector2(Mathf.Clamp01(ratio), fill.anchorMax.y);
+            }
         }
 
-        /// <summary>设置怪物血条比例（0~1，供平滑缓动）</summary
+        /// <summary>设置怪物血条比例（0~1，锚点拉伸法）</summary>
         public static void SetMonsterHpRatio(this BattleInfoPanelComponent self, float ratio)
         {
-            self.u_ComImgMonsterHp.fillAmount = Mathf.Clamp01(ratio);
+            RectTransform fill = self.u_ComImgMonsterHp.transform as RectTransform;
+            if (fill != null)
+            {
+                fill.anchorMax = new Vector2(Mathf.Clamp01(ratio), fill.anchorMax.y);
+            }
         }
 
-        /// <summary>隐藏怪物血条（面板打开默认）</summary>
+        /// <summary>隐藏怪物血条（面板打开默认，隐藏整个容器）</summary>
         public static void HideMonster(this BattleInfoPanelComponent self)
         {
             self.u_ComTextMonsterName.gameObject.SetActive(false);
-            self.u_ComImgMonsterHp.gameObject.SetActive(false);
+            self.u_ComMonsterHpBar.gameObject.SetActive(false);
         }
 
         /// <summary>飘字挂载点（面板所在 Canvas）</summary>
